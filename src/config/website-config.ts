abbc6fcdcfe22026f3548af2dae5be85be0567c0
frostybee <rsleiman2@gmail.com>
2025-08-26 22:58:48 -0400

    Rename base dir

--- src/config/website-config.ts
+++ src/config/website-config.ts
@@ -5,12 +5,12 @@ export default {
   title: 'E-Commerce Course',
 
   // The URI of the site. This is used in the <head> of the page.
-  url: 'https://frostybee.github.io/e-commerce/',
+  url: 'https://frostybee.github.io/web-dev/',
 
   // The base directory of the site. Don't include a trailing slash.
-  baseDirectory: '/e-commerce', // TODO: set the base path for the site if you are deploying to a subdirectory (recommended) on GitHub Pages. The directory name must match the name of the GitHub repo.
+  baseDirectory: '/web-dev', // TODO: set the base path for the site if you are deploying to a subdirectory (recommended) on GitHub Pages. The directory name must match the name of the GitHub repo.
 
-  gitHubRepoUri: 'https://github.com/frostybee/e-commerce',
+  gitHubRepoUri: 'https://github.com/frostybee/web-dev',
 
   // The path to the favicon
   // You can use: https://www.svgrepo.com/
